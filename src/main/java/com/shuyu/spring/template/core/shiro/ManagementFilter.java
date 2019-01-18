450b9646dfe4bd7a68ed91dfcdc2ab4896431fe3
guoshuyu <guoshuyu@ygsoft.com>
2019-01-18 11:05:49 +0800

    actuator接口增加权限判断

--- src/main/java/com/shuyu/spring/template/core/shiro/ManagementFilter.java
+++ src/main/java/com/shuyu/spring/template/core/shiro/ManagementFilter.java
@@ -5,8 +5,8 @@ import org.json.JSONObject;
 
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 /**
@@ -32,25 +32,36 @@ public class ManagementFilter extends JWTFilter {
 
     @Override
     protected boolean executeLogin(ServletRequest request, ServletResponse response) throws Exception {
-        // 读取请求内容
-        BufferedReader br;
-        try {
-            br = new BufferedReader(new InputStreamReader(request.getInputStream(), "utf-8"));
-            String line;
-            StringBuilder sb = new StringBuilder();
-            while ((line = br.readLine()) != null) {
-                sb.append(line);
-            }
-            //将json字符串转换为json对象
-            JSONObject json = new JSONObject(sb.toString());
-            String username = json.getString("username");
-            String password = json.getString("password");
+
+        String method = ((HttpServletRequest)request).getMethod();
+
+        if ("GET".equals(method)) {
+            String username = request.getParameter("username");
+            String password = request.getParameter("password");
             if (managementProperties.getUsername().equals(username) && managementProperties.getPassword().equals(password)) {
                 return true;
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-            throw new UnauthorizedException();
+        } else {
+            // 读取请求内容
+            BufferedReader br;
+            try {
+                br = new BufferedReader(new InputStreamReader(request.getInputStream(), "utf-8"));
+                String line;
+                StringBuilder sb = new StringBuilder();
+                while ((line = br.readLine()) != null) {
+                    sb.append(line);
+                }
+                //将json字符串转换为json对象
+                JSONObject json = new JSONObject(sb.toString());
+                String username = json.getString("username");
+                String password = json.getString("password");
+                if (managementProperties.getUsername().equals(username) && managementProperties.getPassword().equals(password)) {
+                    return true;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new UnauthorizedException();
+            }
         }
         return false;
     }
